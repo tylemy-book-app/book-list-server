ea1c762950dc1179ce6588ee63e38fce47f1f0c1
Tyler Fishbone <tyler.fishbone@gmail.com>
2018-02-28 10:51:10 -0800

    added bodyparser required to server.js, added post route for new books coming in through the form that jeremy is building

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const cors = require('cors');
 const pg = require('pg');
+const bodyParser = require('body-parser').urlencoded({ extended: true });
 
 const app = express();
 const PORT = process.env.PORT;
@@ -14,8 +15,6 @@ client.connect();
 
 app.use(cors());
 
-//app.get('/', (req, res) => res.send('testing 1, 2, 3, 4'));
-
 app.get('/api/v1/books', (req, res) => {
   client.query(
     `SELECT book_id, author, title, image_url FROM books;`)
@@ -25,13 +24,22 @@ app.get('/api/v1/books', (req, res) => {
 
 app.get('/api/v1/books/:id', (req, res) => {
   client.query(
-    `SELECT * FROM books WHERE book_id=$1`,
+    `SELECT * FROM books WHERE book_id=$1;`,
     [req.params.id])
     .then(result => res.send(result.rows))
     .catch(console.error);
 });
 
 app.get('*', (req, res) => res.redirect(CLIENT_URL));
+
+app.post('/api/v1/books', bodyParser, (req, res) => {
+  let {title, author, isbn, image_url, description} = req.body;
+  client.query(`
+  INSERT INTO books(title, author, isbn, image_url, description) VALUES ($1, $2, $3, $4, $5);`,
+  [title, author, isbn, image_url, description])
+    .then(() => res.sendStatus(201))
+    .catch(err => console.error(err));
+});
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
 
